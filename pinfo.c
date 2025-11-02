e4fa061270883a49ef7047646a4159ef0efc1c98
SonnySalerno <sonny.salerno10@gmail.com>
2025-11-01 22:36:27 -0500

    added code for pinfo and pmod

--- pinfo.c
+++ pinfo.c
@@ -1,9 +1,61 @@
 // Name, etc
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <sched.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/resource.h>
 
 // pinfo.c
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+    pid_t pid;
+    int policy;
+    int priority;
+    if (argc > 1){
+        pid = (pid_t)atoi(argv[1]);
+    } else {
+        pid = getpid();
+    }
 
+    errno = 0;
+    priority = getpriority(PRIO_PROCESS, pid);
+    if (errno != 0) {
+        perror("getpriority failed");
+        return 1;
+    }
+
+    policy = sched_getscheduler(pid);
+    if (policy) {
+        perror("sched_getschedular failed");
+        return 1; 
+    }
+    printf("Process ID: %d\n", pid);
+    printf("priority: %d\n", priority);
+
+    switch (policy) {
+        case SCHED_OTHER:
+            printf("Scheduling Policy: SCHED_OTHER\n");
+            break;
+        case SCHED_FIFO:
+            printf("Scheduling Policy: SCHED_FIFO\n");
+            break;
+        case SCHED_RR:
+            printf("Scheduling Policy: SCHED_RR\n");
+            break;
+#ifdef SCHED_BATCH
+        case SCHED_BATCH:
+            printf("Scheduling Policy: SCHED_BATCH\n");
+            break;
+#endif
+#ifdef SCHED_IDLE
+        case SCHED_IDLE:
+            printf("Scheduling Policy: SCHED_IDLE\n");
+            break;
+#endif
+        default:
+            printf("Scheduling Policy: Unknown %d\n", policy);
+    }
     return 0;
 }
--- pmod.c
+++ pmod.c
@@ -1,9 +1,25 @@
 // Name, etc
-
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
 // pmod.c
 
 int main(int argc, char* argv[])
 {
+    int new_nice_value;
+    new_nice_value = nice(10);
 
+    printf("Priority reduced by 10. New nice value is %d\n", new_nice_value);
+
+    struct timespec ts;
+    ts.tv_sec = 1;
+    ts.tv_nsec = 837272638;
+
+    printf("Sleeping for 1,837,272,638 nanoseconds\n");
+    if (nanosleep(&ts, NULL) == -1) {
+        perror("nanosleep");
+        return 1;
+    }
+    printf("Process completed!\n");
     return 0;
 }
